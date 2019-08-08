ea53ae2bfe995fe1b84e10403f21cd90554193df
von Arx  Denis <dvarx@ethz.ch>
2019-08-08 13:22:06 +0200

    added further option for boot repair

--- mbr_fix.sh
+++ mbr_fix.sh
@@ -1,6 +1,11 @@
 sudo add-apt-repository ppa:yannubuntu/boot-repair
+sudo apt update
+sudo apt install boot-loader
 
 sudo apt-get install syslinux
 sudo dd if=/usr/lib/syslinux/mbr.bin of=/dev/sda
 sudo apt-get install mbr
 sudo install-mbr -i n -p D -t 0 /dev/sda
+
+sudo apt-get install lilo
+sudo lilo -M /dev/sda mbr
