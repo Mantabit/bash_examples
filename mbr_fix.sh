2ab4b57294a6fb4f778ab5a91e6b7300bb64eb36
von Arx  Denis <dvarx@ethz.ch>
2019-08-08 13:11:30 +0200

    added script for mbr

---
+++ mbr_fix.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:yannubuntu/boot-repair
+
+sudo apt-get install syslinux
+sudo dd if=/usr/lib/syslinux/mbr.bin of=/dev/sda
+sudo apt-get install mbr
+sudo install-mbr -i n -p D -t 0 /dev/sda
